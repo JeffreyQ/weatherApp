52bd9ce4c3926761cb1fe2a05b4c2cc11fb225d8
Jeffrey Qiu <jeffreyyqiu@gmail.com>
2019-11-09 08:11:38 -0800

    Initial Commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate  {
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var weatherDetails: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            // fetchWeather()
+            guard let locVal: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+            print("locations = \(locVal.latitude) \(locVal.longitude)")
+            
+        }
+    }
 
+    @IBAction func refreshWeather(_ sender: Any) {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+    }
 }
 
